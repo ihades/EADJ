3a21cd59a181e2179ca0a6f4ac6bd8c262c49998
Christoph Bühlmann <christoph@buehlmann.tk>
2015-12-14 22:42:04 +0100

    Fixed Search-Query (Criteria API, YES :-))

--- code/bookstore-persist/src/main/java/org/books/persistence/dao/BookDao.java
+++ code/bookstore-persist/src/main/java/org/books/persistence/dao/BookDao.java
@@ -13,8 +13,6 @@ import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import org.books.persistence.entity.Book;
 import org.books.persistence.entity.Book_;
-import org.books.persistence.dto.BookInfo;
-import static org.books.persistence.dto.BookInfo.QUERY_BOOKS_BY_YEAR;
 
 @Stateless
 @LocalBean
@@ -34,44 +32,30 @@ public class BookDao extends GenericDao<Book> {
                 .getSingleResult();
     }
 
-    public List<Book> searchByKeywords(String[] keywords) {
-
-        CriteriaBuilder cb = getEM().getCriteriaBuilder();
-        CriteriaQuery<Book> cq = cb.createQuery(Book.class);
+    public List<Book> searchByKeywords(List<String> keywords) {
+        CriteriaBuilder qb = getEM().getCriteriaBuilder();
+        CriteriaQuery cq = qb.createQuery();
         Root<Book> book = cq.from(Book.class);
 
-        // Concatinate title, authors and publisher to one string
-        Expression<String> exp1 = cb.concat(book.get(Book_.title), "\t");
-        exp1 = cb.concat(exp1, book.get(Book_.authors));
-        exp1 = cb.concat(exp1, "\t");
-        exp1 = cb.concat(exp1, book.get(Book_.publisher));
-
-        // Add LIKE statement for each keyword
         List<Predicate> predicates = new ArrayList<>();
-        for (String key : keywords) {
-            predicates.add(cb.like(cb.upper(exp1), "%" + key.toUpperCase() + "%"));
+        for (String string : keywords) {
+            predicates.add(
+                    qb.or(new Predicate[]{
+                        qb.like(
+                                qb.lower(book.get(Book_.authors)),
+                                "%" + string.toLowerCase() + "%"),
+                        qb.like(
+                                qb.lower(book.get(Book_.publisher)),
+                                "%" + string.toLowerCase() + "%"),
+                        qb.like(
+                                qb.lower(book.get(Book_.title)),
+                                "%" + string.toLowerCase() + "%")
+                    }));
         }
-
-        cq.select(book).where(cb.or(predicates.toArray(new Predicate[]{})));
+        //query itself
+        cq.select(book).where(predicates.toArray(new Predicate[]{}));
 
         return getEM().createQuery(cq).getResultList();
-    }
-
-    /**
-     * @return The first given number of books of all books ordered by their
-     * publication-year.
-     * @param maxResults Defines the max-size of the return-list (must be bigger
-     * than 0).
-     */
-    public List<BookInfo> findLatestBooks(int maxResults) {
-        if (maxResults <= 0) {
-            throw new IllegalArgumentException("The parameter of the method BookDao.findLatestBooks(..) must be bigger than 0 !!");
-        }
-        return getEM()
-                .createNamedQuery(QUERY_BOOKS_BY_YEAR, BookInfo.class)
-                .setFirstResult(0)
-                .setMaxResults(maxResults)
-                .getResultList();
     }
 
     /**
--- code/bookstore-persist/src/test/java/org/books/persistence/BookDaoTest.java
+++ code/bookstore-persist/src/test/java/org/books/persistence/BookDaoTest.java
@@ -1,9 +1,9 @@
 package org.books.persistence;
 
+import java.util.Arrays;
 import org.books.persistence.testdata.AbstractTestBase;
 import java.util.List;
 import org.books.persistence.dao.BookDao;
-import org.books.persistence.dto.BookInfo;
 import org.books.persistence.entity.Book;
 import static org.junit.Assert.*;
 import org.junit.Test;
@@ -19,75 +19,54 @@ public class BookDaoTest extends AbstractTestBase {
     }
 
     @Test
-    public void testSearchByKeywords() {
-
+    public void testGetById() {
         BookDao bookDao = new BookDao(getEm());
-        List<Book> result1 = bookDao.searchByKeywords(new String[]{"Programmieren", "Philip"});
-        List<Book> result2 = bookDao.searchByKeywords(new String[]{"Habelitz"});
-        List<Book> result3 = bookDao.searchByKeywords(new String[]{"Insel", "Reilly"});
-        List<Book> result4 = bookDao.searchByKeywords(new String[]{"Galileo", "Reilly"});
-        List<Book> result5 = bookDao.searchByKeywords(new String[]{"ckerma"});
-        List<Book> result6 = bookDao.searchByKeywords(new String[]{"rungen", "anse"});
-
-        assertTrue(result1.size() > 0);
-        assertTrue(result2.size() > 0);
-        assertTrue(result3.size() > 0);
-        assertTrue(result4.size() > 0);
-        assertTrue(result5.size() > 0);
-        assertTrue(result6.size() > 0);
-
-        assertEquals("Programmieren mit Java", result1.get(0).getTitle());
-        assertEquals("Java für Dummies", result2.get(0).getTitle());
-        assertEquals("Java Insel", result3.get(0).getTitle());
-
-        assertTrue(result4.size() > 1);
-        assertTrue(checkContainsBook("Java 8 - Die Neuerungen", result4));
-        assertTrue(checkContainsBook("Java Insel", result4));
-
-        assertEquals("Programmieren mit Java", result5.get(0).getTitle());
-
-        assertTrue(result6.size() > 1);
-        assertTrue(checkContainsBook("Java für Dummies", result6));
-        assertTrue(checkContainsBook("Java 8 - Die Neuerungen", result6));
+        Book book = bookDao.getById(9l);
+        assertNotNull(book);
+        assertEquals("Learning Java (3rd Edition)", book.getTitle());
     }
 
     @Test
-    public void testSearchConcatMatch() {
+    public void testNotValidSearch() {
         BookDao bookDao = new BookDao(getEm());
-        List<Book> result = bookDao.searchByKeywords(new String[]{"Holmes", "Learning"});
-        assertTrue(result.isEmpty());
+        List<Book> result1 = searchByKeywords(bookDao, new String[]{"Programmieren", "Philip"});
+        List<Book> result2 = searchByKeywords(bookDao, new String[]{"Habelitz"});
+        List<Book> result3 = searchByKeywords(bookDao, new String[]{"Insel", "Reilly"});
+        List<Book> result4 = searchByKeywords(bookDao, new String[]{"Galileo", "Reilly"});
+        List<Book> result5 = searchByKeywords(bookDao, new String[]{"ckerma"});
+        List<Book> result = searchByKeywords(bookDao, new String[]{"Holmes", "Learning"});
+        assertTrue("Holmes and Learning is not occuring within the same Book", result.isEmpty());
+        assertTrue("Size of \"Programmieren Philip\"", result1.isEmpty());
+        assertTrue("Size of \"Habelitz\"", result2.isEmpty());
+        assertTrue("Size of \"Insel Reilly\"", result3.isEmpty());
+        assertTrue("Size of \"Galileo Reilly\"", result4.isEmpty());
+        assertTrue("Size of \"ckerma\"", result5.isEmpty());
+    }
 
-        result = bookDao.searchByKeywords(new String[]{"Holmes", "Java"});
+    @Test
+    public void testValidSearch() {
+        BookDao bookDao = new BookDao(getEm());
+        List<Book> result = searchByKeywords(bookDao, new String[]{"Holmes", "Java"});
         assertEquals(1, result.size());
+
+        result = searchByKeywords(bookDao, new String[]{"Pro", "W"});
+        assertEquals(1, result.size());
+
+        result = searchByKeywords(bookDao, new String[]{"Pro", "w"});
+        assertEquals(1, result.size());
+
+        result = searchByKeywords(bookDao, new String[]{"Pro", "Java"});
+        assertEquals(2, result.size());
+
+        result = searchByKeywords(bookDao, new String[]{"Da"});
+        assertEquals(4, result.size());
+
+        result = searchByKeywords(bookDao, new String[]{"Da", "NutSHell"});
+        assertEquals(2, result.size());
     }
 
-    @Test
-    public void testLatest3Books() {
-        BookDao bookDao = new BookDao(getEm());
-        List<BookInfo> latestBooks = bookDao.findLatestBooks(3);
-
-        assertNotNull(latestBooks);
-        assertEquals(3, latestBooks.size());
-
-        Integer prevPubYear = 9999;
-        for (BookInfo latestBook : latestBooks) {
-            final Integer pubYear = getEm().find(Book.class, latestBook.getId()).getPublicationYear();
-            assertTrue(getEm().find(Book.class, latestBook.getId()).getPublicationYear() <= prevPubYear);
-            prevPubYear = pubYear;
-        }
+    public List<Book> searchByKeywords(BookDao bookDao, String[] keywords) {
+        return bookDao.searchByKeywords(Arrays.asList(keywords));
     }
 
-    private boolean checkContainsBook(String title, List<Book> bookList) {
-
-        boolean ret = false;
-
-        for (Book book : bookList) {
-            if (title.equals(book.getTitle())) {
-                ret = true;
-                break;
-            }
-        }
-
-        return ret;
-    }
 }
