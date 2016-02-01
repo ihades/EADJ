85dba16c47813b56fcd1c020469f31025c062b55
Christoph Bühlmann <christoph@buehlmann.tk>
2016-02-01 14:07:23 +0100

    implemented Amazon Security-Handler

--- code/bookstore-ejb/src/test/java/org/books/persistence/ejb/AmazonIT.java
+++ code/bookstore-ejb/src/test/java/org/books/persistence/ejb/AmazonIT.java
@@ -3,7 +3,6 @@ package org.books.persistence.ejb;
 import java.math.BigInteger;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-import org.books.ejb.amazon.impl.AwsHandlerResolver;
 import org.books.ejb.amazone.impl.AWSECommerceService;
 import org.books.ejb.amazone.impl.AWSECommerceServicePortType;
 import org.books.ejb.amazone.impl.Item;
@@ -19,25 +18,23 @@ import org.testng.annotations.Test;
  */
 public class AmazonIT {
 
-    private static final String ACCESS_ID = "AKIAIYFLREOYORYNAQTQ";
-    private static final String SECRET_KEY = "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";
-    private static final String ASSOCIATE_TAG = "test0e5d-20";
-
     public AmazonIT() {
     }
 
     @Test
     public void findTest() throws InterruptedException {
 
+        System.setProperty("com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump", "true");
+        System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
+        System.setProperty("com.sun.xml.ws.transport.http.HttpAdapter.dump", "true");
+        System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dump", "true");
+
         AWSECommerceService service = new AWSECommerceService();
-        service.setHandlerResolver(new AwsHandlerResolver(SECRET_KEY));
         AWSECommerceServicePortType port = service.getAWSECommerceServicePort();
         ItemSearchRequest request = new ItemSearchRequest();
         request.setSearchIndex("Books");
         request.setKeywords("Austenland");
         ItemSearch itemSearch = new ItemSearch();
-        itemSearch.setAWSAccessKeyId(ACCESS_ID);
-        itemSearch.setAssociateTag(ASSOCIATE_TAG);
         itemSearch.getRequest().add(request);
         ItemSearchResponse response = port.itemSearch(itemSearch);
         List<Items> itemsList = response.getItems();
@@ -47,8 +44,6 @@ public class AmazonIT {
             TimeUnit.SECONDS.sleep(1);
             request.setItemPage(new BigInteger(String.valueOf(i)));
             itemSearch = new ItemSearch();
-            itemSearch.setAWSAccessKeyId(ACCESS_ID);
-            itemSearch.setAssociateTag(ASSOCIATE_TAG);
             itemSearch.getRequest().add(request);
             response = port.itemSearch(itemSearch);
             itemsList.addAll(response.getItems());
